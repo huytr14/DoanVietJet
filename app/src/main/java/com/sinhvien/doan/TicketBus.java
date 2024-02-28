5893311a6c862bc91cb915545f39e6d2c2e3374f
DESKTOP-FQ3C1PC\KhanhMy <mybui11qwer@gmail.com>
2024-02-28 14:00:50 +0700

    My

---
+++ app/src/main/java/com/sinhvien/doan/TicketBus.java
@@ -0,0 +1,14 @@
+package com.sinhvien.doan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketBus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_bus);
+    }
+}
