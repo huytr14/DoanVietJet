df20c02b1fa6ba775c76a32d2319c703088324ce
DESKTOP-FQ3C1PC\KhanhMy <mybui11qwer@gmail.com>
2024-03-13 00:00:26 +0700

    Update of My

--- app/src/main/java/com/sinhvien/doan/LoginNRegis/SetUpNameUser.java
+++ app/src/main/java/com/sinhvien/doan/LoginNRegis/SetUpNameUser.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,20 +16,29 @@ import com.sinhvien.doan.R;
 public class SetUpNameUser extends AppCompatActivity {
 
     EditText nameUser;
-    Button btnNext;
+    Button btnCreate;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setname);
 
         nameUser = findViewById(R.id.inputName);
-        btnNext = findViewById(R.id.button);
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        progressBar = findViewById(R.id.progressBar);
+        btnCreate = findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                btnCreate.setVisibility(View.INVISIBLE);
                 if(nameUser.getText().toString().length() != 0){
                     Intent i = new Intent(SetUpNameUser.this, Index.class);
                     startActivity(i);
+                }
+                if(nameUser.getText().toString().length() == 0){
+                    progressBar.setVisibility(View.GONE);
+                    btnCreate.setVisibility(View.VISIBLE);
+                    Toast.makeText(SetUpNameUser.this,"Enter your name",Toast.LENGTH_SHORT);
                 }
             }
         });
