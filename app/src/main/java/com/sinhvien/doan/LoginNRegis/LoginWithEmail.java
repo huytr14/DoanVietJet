cb5165336368c906ea7b9f465bfa810173804f1f
tghuy140104@gmail.com <tghuy140104@gmail.com>
2024-03-27 22:04:55 +0700

    My update

--- app/src/main/java/com/sinhvien/doan/LoginNRegis/LoginWithEmail.java
+++ app/src/main/java/com/sinhvien/doan/LoginNRegis/LoginWithEmail.java
@@ -10,10 +10,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
 import com.sinhvien.doan.Admin.IndexAdmin;
 import com.sinhvien.doan.FragmentParents.HomeFragment;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,13 +33,19 @@ import com.google.firebase.auth.FirebaseUser;
 import com.sinhvien.doan.Activity.Index;
 import com.sinhvien.doan.R;
 
+import java.util.HashMap;
+
 public class LoginWithEmail extends AppCompatActivity {
 
     EditText inputEmail, inputPassword;
     Button btnContinue;
     FirebaseAuth firebase;
+    FirebaseDatabase firebaseDatabase;
     ProgressBar progressBar;
     TextView  Create;
+    Button btnGoogle;
+    GoogleSignInClient googleSignInClient;
+    int RC_SIGN_IN=20;
 
     @Override
     public void onStart() {
@@ -53,8 +68,20 @@ public class LoginWithEmail extends AppCompatActivity {
         inputPassword = findViewById(R.id.inputPassword);
         Create = findViewById(R.id.btnCreate);
         firebase = FirebaseAuth.getInstance();
+        firebaseDatabase=FirebaseDatabase.getInstance();
         progressBar = findViewById(R.id.progressBar);
         btnContinue = findViewById(R.id.btnContinue);
+        btnGoogle=findViewById(R.id.btnGoogle);
+        GoogleSignInOptions gso=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail().build();
+        googleSignInClient= GoogleSignIn.getClient(this,gso);
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
         Create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,17 +109,20 @@ public class LoginWithEmail extends AppCompatActivity {
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(inputEmail.getText().toString().equals("admin123")&&inputPassword.getText().toString().equals("toilatamngu")){
+                                if(inputEmail.getText().toString().equals("admin123")&&inputPassword.getText().toString().equals("toiladuccong")){
                                     Intent a=new Intent(LoginWithEmail.this, IndexAdmin.class);
                                     startActivity(a);
                                 }else {
                                     if (task.isSuccessful()) {
-                                        progressBar.setVisibility(View.GONE);
-                                        btnContinue.setVisibility(View.VISIBLE);
-                                        Toast.makeText(LoginWithEmail.this,"Login successful",Toast.LENGTH_SHORT).show();
-                                        Intent intent = new Intent(LoginWithEmail.this, Index.class);
-                                        startActivity(intent);
-                                        finish();
+                                        if(firebase.getCurrentUser().isEmailVerified()){
+                                            progressBar.setVisibility(View.GONE);
+                                            btnContinue.setVisibility(View.VISIBLE);
+                                            Toast.makeText(LoginWithEmail.this,"Login successful",Toast.LENGTH_SHORT).show();
+                                            Intent intent = new Intent(LoginWithEmail.this, Index.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+
                                     } else {
                                         progressBar.setVisibility(View.GONE);
                                         btnContinue.setVisibility(View.VISIBLE);
@@ -100,10 +130,51 @@ public class LoginWithEmail extends AppCompatActivity {
                                         Toast.makeText(LoginWithEmail.this, "Login failed", Toast.LENGTH_SHORT).show();
                                     }
                                 }
-
                             }
                         });
             }
         });
+    }
+    private void googleSignIn(){
+        Intent intent=googleSignInClient.getSignInIntent();
+        startActivityForResult(intent,RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_SIGN_IN){
+            Task<GoogleSignInAccount>task=GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account=task.getResult(ApiException.class);
+                firebaseAuth(account.getIdToken());
+            }
+            catch (Exception e){
+                Toast.makeText(this,e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void firebaseAuth(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebase.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = firebase.getCurrentUser();
+                            HashMap<String, Object> map = new HashMap<>();
+                            map.put("id", user.getUid());
+                            map.put("name", user.getDisplayName());
+                            map.put("profile", user.getPhotoUrl().toString());
+                            firebaseDatabase.getReference().child("Google Login").child(user.getUid()).setValue(map);
+                            Intent intent = new Intent(LoginWithEmail.this, Index.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginWithEmail.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
